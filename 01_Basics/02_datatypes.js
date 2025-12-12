450b359fd8dace63d3d2e57e48980f92aaad66ee
1khushisharma <160408429+1khushisharma@users.noreply.github.com>
2025-12-12 13:25:55 +0000

    datatypes

---
+++ 01_Basics/02_datatypes.js
@@ -0,0 +1,18 @@
+"use Strict"; //treats all the js code as newer version
+
+//aler(3+3); ->its output will be 6 but in browser not here because we are using node js to run js code 
+
+let name="khushi"  //string
+let age=21         //int
+let isLogin=false  //Boolean 
+
+//1)primitive datatypes
+//int
+// bigint 
+// string " "
+// boolean(true or false)
+// null-it is a standalone value
+//undefined
+//symbol(it is used for uniqueness)
+
+//2)object

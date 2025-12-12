bfb686bb8dbb08831a642e310eea41751c27976f
1khushisharma <160408429+1khushisharma@users.noreply.github.com>
2025-12-12 12:58:54 +0000

    changes made

---
+++ 01_Basics/01_variables.js
@@ -0,0 +1,21 @@
+const accountId=144553
+let accountEmail="Khushi@gmail.com"
+var accountPassword="1234"
+accountCity="jaipur"
+let accountState; // value will be Undefined
+
+
+/*
+1)Constant
+  -can not be reassigned
+2)Let
+  - can be reassigned 
+  - it is both block and function scoped
+3)Var  
+  -var is not used because it is not block scope and function scope.
+  -it can be reassigned 
+*/
+
+
+// console.log(accountId);
+console.table([accountId,accountEmail,accountPassword,accountState,accountCity]);

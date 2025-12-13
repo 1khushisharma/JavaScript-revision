e46143b4598d96a4b3b46cf9765a7e82a919920f
1khushisharma <160408429+1khushisharma@users.noreply.github.com>
2025-12-13 04:44:30 +0000

    conversionOperation

---
+++ 01_Basics/03_conversionsOperation.js
@@ -0,0 +1,51 @@
+let score="33"
+let value=Number(score);
+console.log(typeof(value));
+
+let score2="33abcdef"
+let value2=Number(score2);
+console.log(typeof(value2));
+console.log(value2);
+
+let score3=null
+let value3=Number(score3);
+console.log(typeof(value3));
+console.log(value3);
+
+let score4
+let value4=Number(score4);
+console.log(typeof(value4));
+console.log(value4);
+
+//we can use Number,String,Boolean to convert 
+
+//"33"=>33
+//"33abc"=>NaN
+//true=>1,false=>0
+
+
+//------------operations------------
+let val=3;
+let negval=-val
+console.log(negval);
+
+
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+// console.log( (3 + 4) * 5 % 3);
+
+// console.log(+true);
+// console.log(+"");
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter);
